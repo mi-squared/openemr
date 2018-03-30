8111dc4da0a952267669b8945f20a186b57c9be9
daniel <daniel@mi-squared.com>
2018-03-30 11:09:07 -0700

    brought in changes to code_types, but kept original code.  Custom changes are commented out to make reverting easy.

--- custom/code_types.inc.php
+++ custom/code_types.inc.php
@@ -721,7 +721,11 @@ function lookup_code_descriptions($codes,$desc_detail="code_text") {
       }
     }
   }
-  return $code_text;
+     //IBH Custome Code: Changed by sherwin to include the orginal code 5/11/2016
+     //return $code." - ".$code_text;
+
+    //Removed IBH Custom Code and left original.  Text was redundant
+    return $code;
 }
 
 /**

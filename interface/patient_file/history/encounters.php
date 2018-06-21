3e0d1991e2e758974ec9c20d5463a79cff7c9981
daniel <daniel@mi-squared.com>
2018-06-20 18:53:15 -0700

    fixed past encounter bug

--- interface/patient_file/history/encounters.php
+++ interface/patient_file/history/encounters.php
@@ -179,15 +179,15 @@ function generatePageElement($start,$pagesize,$billing,$issue,$text)
 <head>
 <?php html_header_show();?>
 <!-- Main style sheet comes after the page-specific stylesheet to facilitate overrides. -->
-<link rel="stylesheet" href="<?php echo $GLOBALS['webroot'] ?>/library/css/encounters.css" type="text/css">
-<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
-    <script type="text/javascript" src="<?= $_SERVER['CONTEXT_DOCUMENT_ROOT'] ?>/_ibh/js/jquery_latest.min.js"></script>
+<link rel="stylesheet" href="<?= $GLOBALS['webroot'] ?>/library/css/encounters.css" type="text/css">
+<link rel="stylesheet" href="<?= $css_header;?>" type="text/css">
+    <script type="text/javascript" src="<?= $GLOBALS['webroot'] ?>/_ibh/js/jquery_latest.min.js"></script>
 <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/jquery-min-1-2-1/index.js"></script>
 <script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/js/ajtooltip.js"></script>
 
-<script type="text/javascript" src="<?= $_SERVER['CONTEXT_DOCUMENT_ROOT'] ?>/_ibh/js/jquery.tablesort.js"></script>
+<script type="text/javascript" src="/_ibh/js/jquery.tablesort.js"></script>
 <!-- IBF_DEV add link to stylesheet -->
-<link rel="stylesheet" href="<?= $_SERVER['CONTEXT_DOCUMENT_ROOT'] ?>/_ibh/css/encounter.css" type="text/css">
+<link rel="stylesheet" href="<?= $GLOBALS['webroot'] ?>/_ibh/css/encounter.css" type="text/css">
 <script language="JavaScript">
 
 //function toencounter(enc, datestr) {
